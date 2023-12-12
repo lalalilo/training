6996bef24131ff04cd85076c1079b4a58fb1bd77
GregdTd <gregoire@lalilo.com>
2023-12-12 12:37:15 +0100

    step 3 pan gestures

--- expo-training/components/Picture.tsx
+++ expo-training/components/Picture.tsx
@@ -15,6 +15,21 @@ export const Picture: React.FC<PictureProps> = ({ picture }) => {
   const scale = useSharedValue(1);
   const savedScale = useSharedValue(1);
 
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const savedTranslationX = useSharedValue(0);
+  const savedTranslationY = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      translateX.value = savedTranslationX.value + e.translationX;
+      translateY.value = savedTranslationY.value + e.translationY;
+    })
+    .onEnd(() => {
+      savedTranslationX.value = translateX.value;
+      savedTranslationY.value = translateY.value;
+    });
+
   const pinchGesture = Gesture.Pinch()
     .onUpdate((e) => {
       scale.value = savedScale.value * e.scale;
@@ -23,8 +38,14 @@ export const Picture: React.FC<PictureProps> = ({ picture }) => {
       savedScale.value = scale.value;
     });
 
+  const simultaneousGestures = Gesture.Simultaneous(panGesture, pinchGesture);
+
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ scale: scale.value }],
+    transform: [
+      { scale: scale.value },
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+    ],
   }));
 
   if (!picture) {
@@ -33,7 +54,7 @@ export const Picture: React.FC<PictureProps> = ({ picture }) => {
 
   return (
     <View style={styles.container}>
-      <GestureDetector gesture={pinchGesture}>
+      <GestureDetector gesture={simultaneousGestures}>
         <Animated.View style={[styles.box, animatedStyle]}>
           <Image style={styles.image} source={picture} />
         </Animated.View>
