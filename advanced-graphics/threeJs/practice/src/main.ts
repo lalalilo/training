e5d7c8dc81533ffa6c9002c39f94e3940715fa07
rquelen <roman.quelen@renaissance.com>
2025-01-10 15:19:48 +0100

    :sparkles: add FPS stats

--- advanced-graphics/threeJs/practice/src/main.ts
+++ advanced-graphics/threeJs/practice/src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 
 // Scene
 const scene = new THREE.Scene();
@@ -50,6 +51,9 @@ controls.enableDamping = true; // Smooth movement
 controls.dampingFactor = 0.05; // Damping factor
 controls.target.set(cube.position.x, cube.position.y, cube.position.z); // Focus on the cube
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
 const gui = new GUI()
 gui.add(cube.rotation, 'x', 0, 10, 0.01)
 gui.add(cube.rotation, 'y', 0, 10, 0.01)
@@ -59,6 +63,7 @@ gui.add(pointLight.position, 'x', -20, 20).name('Light Pos X')
 function animate() {
     controls.update(); // Required for damping
     renderer.render(scene, camera);
+    stats.update()
 }
 
 // Handle Window Resize
